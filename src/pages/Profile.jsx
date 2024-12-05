c3863c42076cef2226e2aececaac11b72a67be72
mdavidstefan <stef.david.marton@gmail.com>
2024-12-05 15:30:50 +0100

    updated profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,19 +3,35 @@ import { middleStyle } from '../utils'
 import { useContext } from 'react'
 import { UserContext } from '../context/UserContext'
 import { NotFound } from './NotFound'
+import { useForm } from 'react-hook-form';
 
 export const Profile = () => {
     const { user } = useContext(UserContext)
+    const { register, handleSubmit, formState: { errors }, } = useForm({
+        defaultValues: {
+            displayName: user?.displayName,
+        }
+    });
     if (!user) return <NotFound />
+
+    const onSubmit = async (data) => {
+        console.log(data, 'onsubmit');
+    }
 
 
     return (
         <div className='page'>
-            <div className="middleStyle">
-                <h3>Felhasználói fiók beállítás</h3>
-                <form>
-                    <input type="text" name='displayName' />
-                    <input type="file" name='' />
+            <div style={middleStyle}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                    <div>
+                        <input {...register('displayName')} type='text' />
+                    </div>
+                    <div>
+                        <label> Avatar </label>
+                        <input type="file" {...register('file', { required: true })} />
+                    </div>
+                    <input type="submit" />
                 </form>
             </div>
         </div>
